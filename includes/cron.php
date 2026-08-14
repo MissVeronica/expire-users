0db95f25f0ec96f710302aa3dcb0ec65486f03cb
MissVeronica <veronica@tranza.se>
2026-08-14 12:53:51 +0200

    Enhance Expire_Users_Cron with user validation and reminders
    Refactor Expire_Users_Cron class to include user validation and reminder sending functionality.

--- includes/cron.php
+++ includes/cron.php
@@ -1,40 +1,57 @@
 <?php
+
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 class Expire_Users_Cron {
 
 	public function __construct() {
 		add_action( 'expire_user_cron', array( $this, 'do_cron' ) );
-		add_action( 'wp', array( $this, 'schedule_cron' ) );
-
-		// @todo Remove - this is just for testing
-		//add_action( 'init', array( $this, 'do_cron' ) );
-	}
-
-	/**
-	 * Schedule cron
-	 */
-	function schedule_cron() {
-		if ( ! wp_next_scheduled( 'expire_user_cron' ) ) {
-			wp_schedule_event( current_time( 'timestamp' ), 'hourly', 'expire_user_cron' ); // hourly, daily or twicedaily
-		}
 	}
 
 	/**
 	 * Do the scheduler
 	 */
 	function do_cron() {
+
+		global $um_expire_users;
+
 		$maybe_expire_users = Expire_Users_Query::query( array(
 			'expired'              => false,
 			'expired_date'         => current_time( 'timestamp' ),
 			'expired_date_compare' => '<'
 		) );
-		if ( $maybe_expire_users->results > 0 ) {
-			foreach ( $maybe_expire_users->results as $expired_user ) {
+
+		$um_expired_users = $maybe_expire_users->results;
+
+		if ( class_exists( 'UM_Expire_Users' ) && isset( $um_expire_users )) {
+			$um_expired_users = $um_expire_users->um_expire_users_validation( $um_expired_users );
+		}
+
+		if ( count( $um_expired_users ) > 0 ) {
+
+			foreach ( $um_expired_users as $expired_user ) {
+
 				$this_expire_user = new Expire_User( $expired_user->ID );
 				$this_expire_user->expire();
 			}
 		}
-	}
 
+		if ( class_exists( 'UM_Expire_Users' ) && isset( $um_expire_users )) {
+			$um_expire_users->um_expired_users_send_reminders();
+			$recurrence = $um_expire_users->cron_schedule_update_recurrence();
+		}
+
+		if ( wp_next_scheduled( 'expire_user_cron' ) ) {
+			wp_clear_scheduled_hook( 'expire_user_cron' );
+		}
+
+		if ( ! isset( $recurrence['interval'] ) || 
+		     ! isset( $recurrence['display'] ) ||
+			 ! wp_schedule_event( $recurrence['interval'], $recurrence['display'], 'expire_user_cron' ) ) {
+
+			$date = new DateTimeImmutable( 'now', new DateTimeZone( 'UTC' ));
+			wp_schedule_event( $date->getTimestamp() + HOUR_IN_SECONDS, 'hourly', 'expire_user_cron' );
+		}
+	}
 }
+
